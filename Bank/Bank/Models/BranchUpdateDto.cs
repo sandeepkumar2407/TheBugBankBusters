2f50e20ef7f99b52da2b8671e8ed54c66036d7c1
2Shashank <shashankkn1243@gmail.com>
2025-09-28 21:22:19 +0530

    Updated BankController and added dtos

---
+++ Bank/Bank/Models/BranchUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Bank.Models
+{
+    public class BranchUpdateDto
+    {
+        public string ?BranchName { get; set; } = null!;
+
+        public string? Baddress { get; set; }
+
+        public string ?IfscCode { get; set; } = null!;
+    }
+}
---
+++ Bank/Bank/Models/EmpDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Bank.Models
+{
+    public class EmpDto
+    {
+        public string EmpName { get; set; } = null!;
+
+        public string EmpRole { get; set; } = null!;
+
+        public string EmpMobile { get; set; } = null!;
+
+        public string EmpEmail { get; set; } = null!;
+
+        public string EmpPass { get; set; } = null!;
+
+        public int? BranchId { get; set; }
+    }
+}
