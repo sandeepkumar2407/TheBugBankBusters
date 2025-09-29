210b599ed7ff6b7cc7f78ddc1fb44a2d23efe209
2Shashank <shashankkn1243@gmail.com>
2025-09-29 23:14:17 +0530

    Added Managers controllers with few methods

---
+++ Bank/Bank/Models/UserDto.cs
@@ -0,0 +1,42 @@
+﻿namespace Bank.Models
+{
+    public class UserDto
+    {
+        public string Uname { get; set; } = null!;
+
+        public DateOnly DoB { get; set; }
+
+        public string? Uaddress { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string Mobile { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+    public class UserUpdateDto
+    {
+        public string ?Uname { get; set; }
+
+        public string? Uaddress { get; set; }
+
+        public string ?Mobile { get; set; }
+
+        public string ?Email { get; set; }
+    }
+    public class UserGetDto
+    {
+        public int UserId { get; set; }
+        public string Uname { get; set; } = null!;
+
+        public DateOnly DoB { get; set; }
+
+        public string? Uaddress { get; set; }
+
+        public string Gender { get; set; } = null!;
+
+        public string Mobile { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+    }
+}
