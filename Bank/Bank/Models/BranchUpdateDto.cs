719fc8e709872488c0cc00daa9bd9865d94e7490
Sandeep <sandeep240704@gmail.com>
2025-10-30 15:17:29 +0530

    BranchUpdateDto added

---
+++ Bank/Bank/Models/BranchUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Bank.Models
+{
+    public class BranchUpdateDto
+    {
+        public string BranchName { get; set; } = null!;
+        public string? Baddress { get; set; }
+        public string IfscCode { get; set; } = null!;
+    }
+}
